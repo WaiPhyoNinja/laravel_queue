2e029833aae7ade3a34b972858bb0a486b17486a
WaiPhyoNinja <waiphyohlaing445137@gmail.com>
2024-03-11 01:00:14 +0630

    fail queue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use App\Jobs\SendEmailJob;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Queue\Events\JobFailed;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Queue\Events\JobProcessed;
+use App\Notifications\QueueFailedNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +31,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Event::listen(JobProcessed::class, [SendEmailJob::class, 'processed']);
+        // Event::listen(JobProcessed::class, [SendEmailJob::class, 'processed']);
+        Queue::failing(function (JobFailed $event) {
+            $user = User::find(1); // Get the user you want to notify
+            $user->notify(new QueueFailedNotification($event));
+        });
     }
 }
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,4 @@
 <nav class="site-navigation position-relative text-right" role="navigation">
-
     <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
       <li><a href="{{ route('show.time') }}" class="nav-link">Time Lists</a></li>
       <li><a href="{{ route('mail.list') }}" class="nav-link">Mail Lists</a></li>
